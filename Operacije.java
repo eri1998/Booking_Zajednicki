faba4bf6b5ef2762c57cf8c9fd8477f187d5983a
eri57 <55844121+eri57@users.noreply.github.com>
2019-09-26 18:40:47 +0200

    operacije

---
+++ Operacije.java
@@ -0,0 +1,32 @@
+package com.comtrade.operacije;
+
+public class Operacije {
+	public static final int SACUVAJ_KORISNIKA = 1;
+	public static final int LOGIN = 2;
+	public static final int SACUVAJ_DRZAVU = 3;
+	public static final int VRATI_DRZAVU = 4;
+	public static final int SACUVAJ_GRAD = 5;
+	public static final int VRATI_GRAD = 6;
+	public static final int SACUVAJ_ADRESU = 7;
+	public static final int VRATI_ADRESU = 8;
+	public static final int SACUVAJ_HOTEL = 9;
+	public static final int VRATI_HOTEL = 10;
+	public static final int SACUVAJ_SOBU = 11;
+	public static final int VRATI_SOBU = 12;
+	public static final int SACUVAJ_SOBUU = 13;
+	public static final int SACUVAJ_USLUGE = 14;
+	public static final int SACUVAJ_SLIKE = 15;
+	public static final int VRATI_SLIKU = 16;
+	public static final int VRATI_SVE_HOTELE = 17;
+	public static final int HOTEL_ADRESA = 18;
+	public static final int GRAD_HOTEL = 19;
+	public static final int DRZAVA_HOTEL = 20;
+	public static final int HOTEL_PRETRAGA_FULL = 21;
+	public static final int SACUVAJ_REZERVACIJU = 22;
+	public static final int SACUVAJ_SLIKE_SOBA = 23;
+	public static final int VRATI_POSLEDNJU_SOBU = 24;
+	public static final int VRATI_FULL_SOBE = 25;
+	public static final int VRATI_REZERVACIJE = 26;
+	public static final int VRATI_POSLEDNJU_REZERVACIJU = 27;
+
+}
