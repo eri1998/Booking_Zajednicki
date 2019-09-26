330fdad470032c16411ef05abf870e8d8c434437
eri57 <55844121+eri57@users.noreply.github.com>
2019-09-26 18:40:19 +0200

    konstante

---
+++ Konstante.java
@@ -0,0 +1,24 @@
+package com.comtrade.konstante;
+
+public enum Konstante {
+	IP_ADRESA("127.0.0.1"), PORT(9000);
+	private String ip_adresa;
+	private int port;
+
+	private Konstante(String ip_adresa) {
+		this.ip_adresa = ip_adresa;
+	}
+
+	private Konstante(int port) {
+		this.port = port;
+	}
+
+	public String getIp_adresa() {
+		return ip_adresa;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+}
